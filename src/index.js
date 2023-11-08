3d95ee085f33659179810571cc399e5dd4ac46ac
VladaYeshtokina <vlada.yeshtokina@gmail.com>
2023-11-08 15:51:32 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import PropTypes from 'prop-types';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
